ac21da7a0df18f31b9d24ff795acc0ae2a3524a5
Sandeep Bagchi <sandeep.bagchi@gmail.com>
2025-11-26 10:51:11 +0530

    download golang x86 csv

--- go/asm/using_objdump/tool/dump/gen.go
+++ go/asm/using_objdump/tool/dump/gen.go
@@ -4,24 +4,34 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
 const (
-	LLVM_DIR        = ".llvm"
-	BUILD_DIR       = ".llvm/build"
-	TBLGEN_BIN      = ".llvm/build/bin/llvm-tblgen"
-	X86_TD          = ".llvm/llvm/lib/Target/X86/X86.td"
-	INCLUDE_DIR     = ".llvm/llvm/include"
-	X86_INCLUDE_DIR = ".llvm/llvm/lib/Target/X86"
-	FILES_TXT       = "files.txt"
-	X86_JSON        = "x86.json"
-	OPCODES_TXT     = "opcodes.txt"
+	LLVM_DIR         = ".llvm"
+	BUILD_DIR        = ".llvm/build"
+	TBLGEN_BIN       = ".llvm/build/bin/llvm-tblgen"
+	X86_TD           = ".llvm/llvm/lib/Target/X86/X86.td"
+	INCLUDE_DIR      = ".llvm/llvm/include"
+	X86_INCLUDE_DIR  = ".llvm/llvm/lib/Target/X86"
+	FILES_TXT        = "files.txt"
+	X86_JSON         = "x86.json"
+	OPCODES_JSON     = "opcodes.json"
+	GO_DIR           = ".go"
+	X86_CSV_URL      = "https://raw.githubusercontent.com/golang/arch/refs/heads/master/x86/x86.csv"
+	X86_V02_CSV_URL  = "https://raw.githubusercontent.com/golang/arch/refs/heads/master/x86/x86.v0.2.csv"
+	X86_CSV_FILE     = "x86.csv"
+	X86_V02_CSV_FILE = "x86.v0.2.csv"
 )
 
 func join(parts ...string) string {
@@ -78,7 +88,11 @@ func printKind(x *structpb.Value) error {
 
 func getKey(val *structpb.Value, key string) *structpb.Value {
 	if dict := val.GetStructValue(); dict != nil {
-		return dict.GetFields()[key]
+		if dict.Fields != nil {
+			if v, ok := dict.Fields[key]; ok {
+				return v
+			}
+		}
 	}
 	return nil
 }
@@ -95,26 +109,72 @@ func parseJSON() {
 	if err := printKind(value); err != nil {
 		log.Fatalln("Failed to print value kind:", err)
 	}
+
 	if dict := value.GetStructValue(); nil != dict {
-		sb := new(strings.Builder)
-		for key, value := range dict.GetFields() {
-			if strings.HasPrefix(key, "V") {
-				opcode := getKey(value, "Opcode").GetStringValue()
-				sb.WriteString(opcode)
-				sb.WriteString("\n")
+		filtered := make(map[string]*structpb.Value)
+		for key, val := range dict.GetFields() {
+			if opcode := getKey(val, "Opcode"); opcode != nil {
+				filtered[key] = val
+			} else {
+				// fmt.Println("Key: ", key)
 			}
-			// process val in next edits
-			_ = value
 		}
-		err := os.WriteFile(OPCODES_TXT, []byte(sb.String()), 0644)
-		if err != nil {
+		opcodes := structpb.NewStructValue(&structpb.Struct{
+			Fields: filtered,
+		})
+		content := protojson.Format(opcodes)
+		if err := os.WriteFile(X86_JSON, []byte(content), 0644); err != nil {
 			log.Fatalln("Failed to write opcodes.txt:", err)
 		}
+		sizeMB := float64(len(content)) / (1024 * 1024)
+		log.Println("Success: Extracted", len(filtered), fmt.Sprintf("(%.1f MB)", sizeMB))
 	}
 	log.Println("Parsed JSON to structpb.Value successfully")
 }
 
+func httpDownload(url, filename string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+	out, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func downloadCSV() error {
+	dir := GO_DIR
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	urls := []string{
+		X86_CSV_URL,
+		X86_V02_CSV_URL,
+	}
+	filenames := []string{
+		filepath.Join(dir, X86_CSV_FILE),
+		filepath.Join(dir, X86_V02_CSV_FILE),
+	}
+	for i, url := range urls {
+		if err := httpDownload(url, filenames[i]); err != nil {
+			return fmt.Errorf("failed to download %s: %v", url, err)
+		}
+	}
+	return nil
+}
+
 func main() {
+	if err := downloadCSV(); err != nil {
+		log.Fatalln("Failed to download CSVs:", err)
+	}
 	// Check if .llvm exists
 	if _, err := os.Stat(LLVM_DIR); os.IsNotExist(err) {
 		cloneLLVM()
